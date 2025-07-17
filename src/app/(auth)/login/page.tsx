cb9b7bbddcbfbd41e618110f2db881baf4cd5aa5
edison <845292122@qq.com>
2025-07-17 19:54:09 +0800

    登录页面修改

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,34 +7,34 @@ export default function Login() {
   return (
     <form className={cn("flex flex-col gap-6")}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">欢迎回来</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          通过手机号和密码登录您的账号
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Label htmlFor="phone">手机号</Label>
+          <Input id="phone" type="phone" placeholder="请输入手机号" required />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
+            <Label htmlFor="password">密码</Label>
             <a
               href="#"
               className="ml-auto text-sm underline-offset-4 hover:underline"
             >
-              Forgot your password?
+              忘记密码?
             </a>
           </div>
           <Input id="password" type="password" required />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          登录
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or continue with
+            其他登录方式
           </span>
         </div>
         <Button variant="outline" className="w-full">
@@ -44,14 +44,8 @@ export default function Login() {
               fill="currentColor"
             />
           </svg>
-          Login with GitHub
+          通过微信登录
         </Button>
-      </div>
-      <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Sign up
-        </a>
       </div>
     </form>
   )
